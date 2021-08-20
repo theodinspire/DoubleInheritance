5a9600860ed255cc5cad1d3a7ff8871f048afad2
Eric T Cormack <cormack@milyli.com>
2021-08-20 14:12:50 -0500

    Add insertion tests

--- Tests/VehicleTests.cs
+++ Tests/VehicleTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using DoubleInheritance.Locomotion;
+using LinqToDB;
 using LinqToDB.Data;
 using LinqToDB.DataProvider.SqlServer;
 using NUnit.Framework;
@@ -131,6 +134,172 @@ VALUES
 
 			Assert.That(bicycle, Is.Not.Null);
 			Assert.That(bicycle, Is.TypeOf(typeof(Bicycle)));
+		}
+
+		[Test]
+		public void CanInsertBicycle()
+		{
+			var bicycle = new Bicycle()
+			{
+				BicycleType = BicycleType.Cargo
+			};
+
+			bicycle.Id = this.connection.InsertWithInt32Identity(bicycle);
+
+			Assert.That(bicycle.Id, Is.Not.EqualTo(default(int)));
+
+			var readBicycle = this.connection.GetTable<Bicycle>().FirstOrDefault(x => x.Id == bicycle.Id);
+			Assert.That(readBicycle, Is.Not.Null);
+			Assert.That(readBicycle.BicycleType, Is.EqualTo(bicycle.BicycleType));
+			// Passes
+		}
+
+		[Test]
+		public void CanInsertBicycleAsVehicle()
+		{
+			Vehicle bicycle = new Bicycle()
+			{
+				BicycleType = BicycleType.Cargo
+			};
+
+			bicycle.Id = this.connection.InsertWithInt32Identity(bicycle);
+
+			Assert.That(bicycle.Id, Is.Not.EqualTo(default(int)));
+			// Passes
+		}
+
+		[Test]
+		public void CanInsertLorry()
+		{
+			var lorry = new Lorry()
+			{
+				CargoCapacity = 10.34,
+			};
+
+			lorry.Id = this.connection.InsertWithInt32Identity(lorry);
+
+			Assert.That(lorry.Id, Is.Not.EqualTo(default(int)));
+			// Passes
+		}
+
+		[Test]
+		public void CanInsertMixedVehicleCollection()
+		{
+			var bicycle = new Bicycle()
+			{
+				BicycleType = BicycleType.Cargo
+			};
+			var lorry = new Lorry()
+			{
+				CargoCapacity = 10.34,
+			};
+
+			var list = new List<Vehicle>(2)
+			{
+				bicycle,
+				lorry,
+			};
+
+			var objectsInserted = this.connection.BulkCopy(list).RowsCopied;
+			// System.InvalidCastException : Unable to cast object of type 'DoubleInheritance.Locomotion.Bicycle' to type 'DoubleInheritance.Locomotion.Automobile'.
+
+			Assert.That(objectsInserted, Is.EqualTo(list.Count));
+		}
+
+		[Test]
+		public void CanInsertMixedAutomobileCollection()
+		{
+			var car = new Car()
+			{
+				MaxOccupancy = 2,
+			};
+			var lorry = new Lorry()
+			{
+				CargoCapacity = 10.34,
+			};
+
+			var list = new List<Automobile>(2)
+			{
+				car,
+				lorry,
+			};
+
+			var objectsInserted = this.connection.BulkCopy(list).RowsCopied;
+			// System.InvalidCastException : Unable to cast object of type 'DoubleInheritance.Locomotion.Car' to type 'DoubleInheritance.Locomotion.Lorry'
+
+			Assert.That(objectsInserted, Is.EqualTo(list.Count));
+		}
+
+		[Test]
+		public void CanInsertBicycleCollection()
+		{
+			var foo = new Bicycle()
+			{
+				BicycleType = BicycleType.Recumbent,
+			};
+			var bar = new Bicycle()
+			{
+				BicycleType = BicycleType.Standard,
+			};;
+
+			var list = new List<Bicycle>(2)
+			{
+				foo,
+				bar,
+			};
+
+			var objectsInserted = this.connection.BulkCopy(list).RowsCopied;
+
+			Assert.That(objectsInserted, Is.EqualTo(list.Count));
+			// Passes
+		}
+
+		[Test]
+		public void CanInsertCarCollection()
+		{
+			var foo = new Car()
+			{
+				MaxOccupancy = 36,
+			};
+			var bar = new Car()
+			{
+				MaxOccupancy = 4,
+			};;
+
+			var list = new List<Car>(2)
+			{
+				foo,
+				bar,
+			};
+
+			var objectsInserted = this.connection.BulkCopy(list).RowsCopied;
+
+			Assert.That(objectsInserted, Is.EqualTo(list.Count));
+			// Passes
+		}
+
+		[Test]
+		public void CanInsertBicycleAsVehicleCollection()
+		{
+			var foo = new Bicycle()
+			{
+				BicycleType = BicycleType.Recumbent,
+			};
+			var bar = new Bicycle()
+			{
+				BicycleType = BicycleType.Standard,
+			};;
+
+			var list = new List<Vehicle>(2)
+			{
+				foo,
+				bar,
+			};
+
+			var objectsInserted = this.connection.BulkCopy(list).RowsCopied;
+			// System.InvalidCastException : Unable to cast object of type 'DoubleInheritance.Locomotion.Bicycle' to type 'DoubleInheritance.Locomotion.Automobile'.
+
+			Assert.That(objectsInserted, Is.EqualTo(list.Count));
 		}
 	}
 }
